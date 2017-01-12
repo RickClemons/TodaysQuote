447f62b668427a4e0c9f323630f407ae07008f92
Charles Clemons <Charles@charless-mbp.cclemons.com>
2017-01-12 11:41:25 -0500

    Initial Commit

---
+++ Clemons_TodaysQuote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clemons_TodaysQuote
+//
+//  Created by Charles Clemons on 1/12/17.
+//  Copyright © 2017 University of Cincinnati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
